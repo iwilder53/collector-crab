3c8e19f1cf51fd9cca2bb3a08223493d46d424ce
iwilder53 <71007725+iwilder53@users.noreply.github.com>
2020-12-31 23:50:33 +0530

    Add files via upload

---
+++ PauseScreen.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseScreen : MonoBehaviour
+{
+	public LevelManager gameLevelManager;
+	public PauseMenu PauseMenuScreen;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         FindObjectOfType<LevelManager>() ;   }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+  public void ResumeGame()
+    {
+        FindObjectOfType<PauseMenu>().RestartGame();
+
+    }
+
+  public void QuitGame()
+ {
+     Debug.Log("QUIT!");
+     Application.Quit();
+ }
+
+
+ public void RestartGame()
+{
+	
+	 gameLevelManager.coins = 0;
+	
+        gameLevelManager.coinText.text = "Score: " + gameLevelManager.coins;	
+
+
+
+	 FindObjectOfType<PauseMenu>().RestartGame();
+
+
+
+}
+
+
+
+
+
+}
