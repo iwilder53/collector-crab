53f7ae98ad20e90bdaf4ea9ddf85d66e44dda7c4
iwilder53 <71007725+iwilder53@users.noreply.github.com>
2020-12-31 23:48:37 +0530

    Add files via upload

---
+++ LevelGenerator.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{	
+	public float RandomUpperLimit;
+	public CoinGenerator coinGenerator;
+    public float randomCoinThreshold;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+            if (Random.Range(0f, RandomUpperLimit) < randomCoinThreshold)
+            {
+                coinGenerator.SpawnCoins(new Vector3(transform.position.x, transform.position.y + 2f, -2f));
+            }
+       
+
+    }
+}
