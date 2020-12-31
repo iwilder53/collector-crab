111d39b83e0421973d44477f758add3252c2398a
iwilder53 <71007725+iwilder53@users.noreply.github.com>
2020-12-31 23:50:18 +0530

    Add files via upload

---
+++ PauseMenu.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+
+{	
+
+
+	public GameObject PauseMenuScreen;
+	public GameObject DeathMenuScreen;
+	public GameObject pooler;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+
+	
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+
+
+
+ public void PauseGame()
+    {
+              Time.timeScale = 0f;
+			pooler.SetActive(false);
+
+		PauseMenuScreen.SetActive(true);
+    }
+	public void RestartGame()
+    {
+              Time.timeScale = 1f;
+		PauseMenuScreen.SetActive(false);
+		DeathMenuScreen.SetActive(false);
+				pooler.SetActive(true);
+
+    }
+
+}
